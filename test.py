3b10941b20409cf3106579addb55342a3b2b1a03
Piyawat-KPY <piyawat.klai@gmail.com>
2026-03-06 00:21:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print ("Hello, World!")
+import os
+stock = []
+with open('CprE_Subject - CprE_Subject.csv') as f:
+    for line in f:
+        stock.append(line.strip().split(','))
+
+for idx, item in enumerate(stock):
+    print(idx, item)
+
